3086f67910a6348c44ec60aaead254e5bd3d862c
Mikhail Tkachenko <m.tkachenko@aimprosoft.com>
2020-08-07 21:11:01 +0300

    base product styles

--- src/app/components/catalogue/catalogue.store.ts
+++ src/app/components/catalogue/catalogue.store.ts
@@ -7,7 +7,7 @@ let seq = 0;
 while (catalogueStore.length < 10) {
   let category = {
     id: seq++,
-    name: `cat ${catalogueStore.length}`,
+    name: `Категория №${catalogueStore.length + 1}`,
     images: [],
     products: []
   };
@@ -15,7 +15,7 @@ while (catalogueStore.length < 10) {
   while (category.products.length < 10) {
     let product = {
       id: seq++,
-      name: `product ${catalogueStore.length}${category.products.length}`,
+      name: `Продукт №${catalogueStore.length + 1}${category.products.length + 1}`,
       price: Math.random() * 300,
       images: [`assets/mock-images/${Math.floor(Math.random() * 7) + 1}.jpg`]
     };
