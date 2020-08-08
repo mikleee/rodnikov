ffc2ea120676e7c5cfe8fab0a24a5905fe914a35
Mikhail Tkachenko <m.tkachenko@aimprosoft.com>
2020-08-08 16:15:58 +0300

    mock data

--- src/app/components/catalogue/catalogue.store.ts
+++ src/app/components/catalogue/catalogue.store.ts
@@ -1,4 +1,4 @@
-import {ProductCategory} from "./catalogue.model";
+import {Product, ProductCategory, ProductSubCategory} from "./catalogue.model";
 
 
 export const catalogueStore: ProductCategory[] = [];
@@ -7,36 +7,77 @@ function imageUrl() {
   return `assets/mock-images/${Math.floor(Math.random() * 14) + 1}.jpg`
 }
 
+let config = [
+  {category: 'Двери', subcategories: ['Входные двери', 'Межкомнатные двери']},
+  {category: 'Напольные покрытия', subcategories: ['Плитка', 'Ламинат', 'Паркет', 'Солома']},
+  {category: 'Гомна на окна', subcategories: ['Роллеты', 'Жалюзи', 'Шторы', 'Гардины']},
+  {category: 'Настенные покрытия', subcategories: ['Обои', 'Краска', 'Кирпич']}
+];
+
+
 let seq = 0;
-while (catalogueStore.length < 10) {
-  let category = {
-    id: seq++,
-    name: `Категория №${catalogueStore.length + 1}`,
-    image: imageUrl(),
-    subcategories: []
-  };
+
+config.forEach((c) => {
+  let category = createProductCategory(c.category);
+  catalogueStore.push(category);
+
+  c.subcategories.forEach((sc) => {
+    let sub = createProductSubCategory(sc, category);
+    category.subcategories.push(sub);
+
+    while (sub.products.length < 10) {
+      let product = createProduct(`${sc} №${sub.products.length+1}`, sub);
+      sub.products.push(product);
+    }
+  });
+
+});
+debugger;
+
+/*while (catalogueStore.length < 10) {
+  let category = createProductCategory(`Категория №${catalogueStore.length + 1}`);
+  catalogueStore.push(category);
+
 
   while (category.subcategories.length < 10) {
-    let sub = {
-      id: seq++,
-      name: `Подкатегория №${catalogueStore.length + 1}${category.subcategories.length + 1}`,
-      category: category,
-      image: imageUrl(),
-      products: []
-    };
+    let sub = createProductSubCategory(`Подкатегория №${catalogueStore.length + 1}${category.subcategories.length + 1}`, category);
     category.subcategories.push(sub);
 
 
     while (sub.products.length < 10) {
-      let product = {
-        id: seq++,
-        subcategory: sub,
-        name: `Продукт №${catalogueStore.length + 1}${category.subcategories.length + 1}${sub.products.length + 1}`,
-        price: Math.random() * 300,
-        images: [imageUrl(), imageUrl(), imageUrl()]
-      };
+      let product = createProduct(`Продукт №${catalogueStore.length + 1}${category.subcategories.length + 1}${sub.products.length + 1}`, sub);
       sub.products.push(product);
     }
   }
   catalogueStore.push(category)
+}*/
+
+
+function createProductCategory(name: string): ProductCategory {
+  return {
+    id: seq++,
+    name: name,
+    image: imageUrl(),
+    subcategories: []
+  };
+}
+
+function createProductSubCategory(name: string, category: ProductCategory): ProductSubCategory {
+  return {
+    id: seq++,
+    name: name,
+    category: category,
+    image: imageUrl(),
+    products: []
+  };
+}
+
+function createProduct(name: string, subcategory: ProductSubCategory): Product {
+  return {
+    id: seq++,
+    subcategory: subcategory,
+    name: name,
+    price: Math.random() * 300,
+    images: [imageUrl(), imageUrl(), imageUrl()]
+  };
 }
